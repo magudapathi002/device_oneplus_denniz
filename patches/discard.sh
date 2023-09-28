9793fe6905199b9159fb1859826dfe73522da816
lahaina <nattolecats@gmail.com>
2023-09-28 22:33:22 +0900

    denniz: patches: Refactor scripts * [Apply / Discard]: Make bashable directly from croot directory. * [Discard]: Increase target branch to 2by2 and Arrow. When first git reset --hard fails automatically fallback to second branch and redo it.

--- patches/discard.sh
+++ patches/discard.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+DEVICE=denniz
+VENDOR=oneplus
+
 export originalPath=$(pwd)
-export branch="arrow/arrow-13.1"
+export branch1="2by2/13"
+export branch2="arrow/arrow-13.1"
+
+if [ $(pwd) == "${ANDROID_BUILD_TOP}" ] ; then export originalPath=$(pwd)/device/$VENDOR/$DEVICE/patches/ ; fi
 
 discardPatches() {
+    cd $originalPath
     for i in $(ls -d */); do
         cd $originalPath
         path=$(tr _ / <<< "$i")
         cd ./../../../../"$path"
         
-        git reset --hard $branch
+        git reset --hard $branch1 2>/dev/null || git reset --hard $branch2 2>/dev/null
     done
 }
 
